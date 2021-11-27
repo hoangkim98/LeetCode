8e13a70c65d4a8f7ca67dda2e0ea68a07c6164af
hoangkimcse <le.kim@digicommerce.com>
2021-11-27 16:58:32 +0700

    nab test 27-11

---
+++ src/nab/Ques1.java
@@ -0,0 +1,41 @@
+package nab;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+
+public class Ques1
+{
+	public static String solution(String message, int K) {
+		// write your code in Java SE 8
+		if(K>= message.length() || message.length() == 0){
+			return message;
+		}
+		String[] strArr = message.split(" ");
+		Map<String, Integer> mapStr = new LinkedHashMap<>();
+		for(int i = 0;i<strArr.length;i++){
+			mapStr.put(strArr[i], strArr[i].length());
+		}
+
+		int count = 0;
+		StringBuilder sb = new StringBuilder();
+
+		for(Map.Entry<String, Integer> entry: mapStr.entrySet()){
+			if(count + entry.getValue() <= K){
+				count += entry.getValue() + 1;
+				sb.append(entry.getKey());
+				sb.append(" ");
+			}
+		}
+
+		return sb.toString().replaceAll("\\s+$", "");
+	}
+
+	public static void main(String[] args)
+	{
+		String test = "abc xyz";
+		String res = solution(test, 3);
+		System.out.println(res);
+
+	}
+}
