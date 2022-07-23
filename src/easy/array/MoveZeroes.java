191833e00ad76e01e4810cf884adb9f57c72a41b
hoangkimcse <le.kim@digicommerce.com>
2022-07-23 15:25:36 +0700

    remove zero

---
+++ src/easy/array/MoveZeroes.java
@@ -0,0 +1,25 @@
+package easy.array;
+
+public class MoveZeroes {
+    public static void moveZeroes(int[] nums) {
+        int zeroIdx = -1;
+        for (int i = 0; i < nums.length; i++) {
+            if (0 != nums[i]) {
+                zeroIdx++;
+                nums[zeroIdx] = nums[i];
+            }
+        }
+
+        for (int i = zeroIdx + 1; i < nums.length; i++) {
+            nums[i] = 0;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {2, 7, 0, 15, 0, 20};
+        moveZeroes(nums);
+        for (int i = 0; i < nums.length; i++) {
+            System.out.print(nums[i] + " ");
+        }
+    }
+}
