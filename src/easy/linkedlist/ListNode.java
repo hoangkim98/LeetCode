8a301ebfc634a9ed663aad63bb2243abc7c8138b
Hoang Kim <kim.le1@mservice.com.vn>
2023-05-12 09:27:16 +0700

    linked list

--- src/easy/linkedlist/ListNode.java
+++ src/easy/linkedlist/ListNode.java
@@ -1,5 +1,7 @@
 package easy.linkedlist;
 
+import java.util.List;
+
 public class ListNode {
     int val;
     ListNode next;
@@ -14,6 +16,33 @@ public class ListNode {
     ListNode(int val, ListNode next) {
         this.val = val;
         this.next = next;
+    }
+
+    public static ListNode genInput(List<Integer> arr){
+        ListNode head = new ListNode(arr.get(0));
+        ListNode temp = head;
+        for (int i = 1; i < arr.size(); i++) {
+            ListNode curr = new ListNode(arr.get(i));
+            temp.next = curr;
+            temp = temp.next;
+        }
+        return head;
+    }
+
+    public static ListNode genCircleLL(List<Integer> arr, int pos){
+        ListNode head = new ListNode(arr.get(0));
+        ListNode tail = head;
+        ListNode circle = null;
+        for (int i = 1; i < arr.size(); i++) {
+            ListNode curr = new ListNode(arr.get(i));
+            tail.next = curr;
+            tail = tail.next;
+            if(pos == i){
+                circle = tail;
+            }
+        }
+        tail.next = circle;
+        return head;
     }
 
 }
