859a4bdf312b7bddf7199059f2e946b606348ee4
hoangkimcse <dev8@digicommercegroup.com>
2023-05-14 17:15:14 +0700

    update

--- src/easy/array/FirstBadVersion.java
+++ src/easy/array/FirstBadVersion.java
@@ -12,11 +12,27 @@ Output: [1,3,6,10]
 Explanation: Running sum is obtained as follows: [1, 1+2, 1+2+3, 1+2+3+4].
  */
 
+/*
+Suppose you have n versions [1, 2, ..., n] and you want to find out the first bad one, which causes all the following ones to be bad.
+
+You are given an API bool isBadVersion(version) which returns whether version is bad. Implement a function to find the first bad version. You should minimize the number of calls to the API.
+
+
+
+Example 1:
+
+Input: n = 5, bad = 4
+Output: 4
+Explanation:
+call isBadVersion(3) -> false
+call isBadVersion(5) -> true
+call isBadVersion(4) -> true
+Then 4 is the first bad version.
+ */
 package easy.array;
 
-public class BinarySearch {
-    public static int search(int[] nums, int target) {
-        return searchRange(nums, 0, nums.length - 1, target);
+public class FirstBadVersion {
+    public int firstBadVersion(int n) {
 
     }
 
@@ -32,6 +48,10 @@ public class BinarySearch {
         } else {
             return searchRange(nums, mid + 1, right, target);
         }
+    }
+
+    public static boolean isBadVersion(int n) {
+        return true;
     }
 
     public static void main(String[] args) {
