fc19664cdc32b0fee06f3bd1e22011de7b06f474
hoangkimcse <le.kim@digicommerce.com>
2023-06-01 20:57:15 +0700

    update

--- src/easy/linkedlist/ListNode.java
+++ src/easy/linkedlist/ListNode.java
@@ -3,13 +3,13 @@ package easy.linkedlist;
 import java.util.List;
 
 public class ListNode {
-    int val;
-    ListNode next;
+    public int val;
+    public ListNode next;
 
-    ListNode() {
+    public ListNode() {
     }
 
-    ListNode(int val) {
+    public ListNode(int val) {
         this.val = val;
     }
 
@@ -18,7 +18,7 @@ public class ListNode {
         this.next = next;
     }
 
-    public static ListNode genInput(List<Integer> arr){
+    public static ListNode genInput(List<Integer> arr) {
         ListNode head = new ListNode(arr.get(0));
         ListNode temp = head;
         for (int i = 1; i < arr.size(); i++) {
@@ -29,7 +29,7 @@ public class ListNode {
         return head;
     }
 
-    public static ListNode genCircleLL(List<Integer> arr, int pos){
+    public static ListNode genCircleLL(List<Integer> arr, int pos) {
         ListNode head = new ListNode(arr.get(0));
         ListNode tail = head;
         ListNode circle = null;
@@ -37,12 +37,19 @@ public class ListNode {
             ListNode curr = new ListNode(arr.get(i));
             tail.next = curr;
             tail = tail.next;
-            if(pos == i){
+            if (pos == i) {
                 circle = tail;
             }
         }
         tail.next = circle;
         return head;
+    }
+
+    public static void printLL(ListNode head) {
+        while (head != null) {
+            System.out.println(head.val);
+            head = head.next;
+        }
     }
 
 }
